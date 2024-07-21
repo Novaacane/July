35b3d32a0367ab850c37a892077cf23668722818
Parul Vats <165577838+Novaacane@users.noreply.github.com>
2024-07-21 23:21:01 +0530

    Add files via upload

---
+++ Assignmentjs1.js
@@ -0,0 +1,33 @@
+var a=5;
+var b=8;
+console.log(a+b);
+
+var c=17;
+var d=18;
+
+console.log(c<10);
+console.log(d>18);
+
+var e=80;
+var f=80;
+console.log(e%10==0 && f%10==0);
+console.log(e%10!==0 && f%10!==0);
+console.log(e%10==0 || f%10==0);
+
+var g=8974;
+console.log(Math.floor(g/1000));
+console.log()
+
+var h=5;
+var j=26;
+console.log((j%h));
+
+var k=78;
+var l=2;
+console.log(k*l);
+
+var x=56;
+var y=89;
+var z=92;
+console.log(x+y+z);
+console.log((x+y+z)/3);
